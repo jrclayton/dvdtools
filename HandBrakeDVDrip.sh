0b3f2b4d865d4203c66b73643b7f94af21fe6609
John "Randy" Clayton <jrclayton@yahoo.com>
2017-02-10 12:18:19 -0800

    fixed some spacing for readability

--- HandBrakeDVDrip.sh
+++ HandBrakeDVDrip.sh
@@ -4,14 +4,14 @@
 # Script based on my GUI preset 
 
 # Check for arguments
-if  [[ $# -eq '0' ]] || [[ $# -eq '1' ]] || [[ $# -eq '2' ]] ||
-    [[ $1 = '-a' && $# -ne '3' && $# -ne '4' ]] ||
-    [[ $1 = '-s' && $# -ne '3' && $# -ne '4' ]] ||
-    [[ $1 = '-i' && $# -ne '4' && $# -ne '5' ]] ||
-    [[ $1 = '--iso' && $# -ne '4' && $# -ne '5' ]] ||
-    [[ $1 = '-t' && $# -ne '5' && $# -ne '6' ]] ||
-    [[ $1 = '-c' && $# -ne '6' && $# -ne '7' ]] ||
-    [[ $1 = '-cf' && $# -ne '6' && $# -ne '7' ]]
+if [[ $# -eq '0' ]] || [[ $# -eq '1' ]] || [[ $# -eq '2' ]] ||
+	[[ $1 = '-a' && $# -ne '3' && $# -ne '4' ]] ||
+	[[ $1 = '-s' && $# -ne '3' && $# -ne '4' ]] ||
+	[[ $1 = '-i' && $# -ne '4' && $# -ne '5' ]] ||
+	[[ $1 = '--iso' && $# -ne '4' && $# -ne '5' ]] ||
+	[[ $1 = '-t' && $# -ne '5' && $# -ne '6' ]] ||
+	[[ $1 = '-c' && $# -ne '6' && $# -ne '7' ]] ||
+	[[ $1 = '-cf' && $# -ne '6' && $# -ne '7' ]]
 then
 	echo -e "Usage: dvdrip [switch] MOVIENAME YEAR ... \n \
 	Select mode: either -a for all, -s to scan, -t for titles or -c for chapters or -i to change input source (add 'f' to merge chapters) \n \
@@ -34,7 +34,7 @@ then
 # if other than -i; then $2 $3
 elif ! [[ $1 = '-i' ]] && ! [[ $1 = '-s' ]]
 then
-    mkdir ~/Desktop/"$2"\ \($3\)
+	mkdir ~/Desktop/"$2"\ \($3\)
 fi
 # if -i; no folder
 
@@ -47,7 +47,7 @@ then
 	# range of chapters to rip returned from $5 and $6
 	for i in `seq $5 $6`
 	do
-	HandBrakeCLI --verbose=1 --markers --title $4 --chapters $i --min-duration 4 --format av_mkv --encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $7 -i $DVD -o ~/Desktop/"$2"\ \($3\)/"$2"\ \($3\)\ -\ $4-$i.mkv
+		HandBrakeCLI --verbose=1 --markers --title $4 --chapters $i --min-duration 4 --format av_mkv --encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $7 -i $DVD -o ~/Desktop/"$2"\ \($3\)/"$2"\ \($3\)\ -\ $4-$i.mkv
 	done
 
 	# Notifications
@@ -76,7 +76,7 @@ then
 	# range of titles to convert returned from $4 and $5	
 	for i in `seq $4 $5`
 	do 
-	HandBrakeCLI --verbose=1 --markers --title $i --min-duration 4 --format av_mkv	--encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $6 -i $DVD -o ~/Desktop/"$2"\ \($3\)/"$2"\ \($3\)\ -\ $i.mkv
+		HandBrakeCLI --verbose=1 --markers --title $i --min-duration 4 --format av_mkv	--encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $6 -i $DVD -o ~/Desktop/"$2"\ \($3\)/"$2"\ \($3\)\ -\ $i.mkv
 	done
 
 	# Notifications
