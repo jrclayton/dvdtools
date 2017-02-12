692297cdbb58947834427f5b598e3e27535330b9
John R. Clayton <jrclayton@yahoo.com>
2017-02-11 18:29:55 -0800

    Cleanup for readability

--- HandBrakeDVDrip.sh
+++ HandBrakeDVDrip.sh
@@ -25,8 +25,17 @@ then
 	exit 1
 fi
 
-# make an appropriately named folder on the desktop
+# identify the proper disk drive du moment
+DVD=$(mount | grep udf | cut -d ' ' -f 1)
 
+# test for a DVD
+if [ -z "$DVD" ]
+then
+    echo "There is no disc mounted."
+    exit 1
+fi
+
+# make an appropriately named folder on the desktop
 # if --iso; then $3 $4
 if [[ $1 = '--iso' ]]
 then
@@ -38,8 +47,7 @@ then
 fi
 # if -i; no folder
 
-# identify the proper disk drive du moment
-DVD=$(mount | grep udf | cut -d ' ' -f 1)
+### RIPPING OPTIONS ###
 
 # To rip individual chapters from a single title into separate files
 if [ $1 = '-c' ]
@@ -86,10 +94,21 @@ then
 	exit 0
 fi
 
-# The following options require a scan
+# To convert a FILE to mkv using template settings
+if [ $1 = '-i' ]; then
 
-# First get the number of titles on the disk by doing a scan (HB CLI and -t 0)
-# and send STDERR to STDIN so it can be grepped
+HandBrakeCLI --verbose=1 --markers --min-duration 4 --format av_mkv --encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $5 -i "$2" -o ~/Desktop/"$3"\ -\ "$4".mkv
+
+# Notifications
+echo "Finished converting $3 to MKV"
+terminal-notifier -message "Finished converting $3 to MKV" -title "Terminal" -subtitle "dvdrip" -sound default -appIcon Documents/Personal_Documents/icons/dvdrip-2.png
+say -v Samantha "Finished converting $3 to MKV"
+exit 0
+fi
+
+# The following options require a scan before ripping
+
+# First get the number of titles on the disk by doing a scan (HB CLI and -t 0) and send STDERR to STDIN so it can be grepped
 RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i $DVD 2>&1)
 
 # output of the scan option
@@ -99,10 +118,13 @@ if [ $1 = '-s' ]; then
 	exit 0
 fi
 
+# Generate numerical list of titles for loops
+TITLES=$(echo $RAWOUT | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3)
+
 # To rip ALL titles
 if [ $1 = '-a' ]; then
 	# return a list of usable titles to plug into a for loop	
-	for i in `echo $RAWOUT | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3`
+	for i in $TITLES
 	
 	do 
 		HandBrakeCLI --verbose=1 --markers --title $i --min-duration 4 --format av_mkv	--encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $4 -i $DVD -o ~/Desktop/"$2"\ \($3\)/"$2"\ \($3\)\ -\ $i.mkv
@@ -115,22 +137,10 @@ if [ $1 = '-a' ]; then
 	exit 0
 fi
 
-# To convert a FILE to mkv using template settings
-if [ $1 = '-i' ]; then
-	
-	HandBrakeCLI --verbose=1 --markers --min-duration 4 --format av_mkv --encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $5 -i "$2" -o ~/Desktop/"$3"\ -\ "$4".mkv
-    	
-	# Notifications
-	echo "Finished converting $3 to MKV"
-	terminal-notifier -message "Finished converting $3 to MKV" -title "Terminal" -subtitle "dvdrip" -sound default -appIcon Documents/Personal_Documents/icons/dvdrip-2.png
-	say -v Samantha "Finished converting $3 to MKV"
-	exit 0
-fi
-
 # To rip all titles from a VIDEO_TS or .ISO file
 if [ $1 = '--iso' ]; then
 	# return a list of usable titles to plug into a for loop
-	for i in `echo $RAWOUT | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3`
+	for i in $TITLES
 	
 	do
 		HandBrakeCLI --verbose=1 --markers --title $i --min-duration 4 --format av_mkv	--encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $5 -i $2 -o ~/Desktop/"$3"\ \($4\)/"$3"\ \($4\)\ -\ $i.mkv
