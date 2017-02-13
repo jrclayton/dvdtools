b9d404bcaff856b8768ce28a3aca09e71e68fda3
John R. Clayton <jrclayton@yahoo.com>
2017-02-12 16:58:17 -0800

    did come cleanup. added

--- dvd2iso.sh
+++ dvd2iso.sh
@@ -1,45 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# dd will make byte for byte copies of, for example, the system disk1,
-# which is the DVD/CD-ROM Drive
-# This method won't work for encrypted discs, which means most movies.
-# So only use this if the disc isn't copy protected
-#sudo umount /dev/disk1
-#dd if=/dev/disk1 of=~/Desktop/myimage.iso
+### dd will make byte for byte copies
+### This method won't work for encrypted discs, which means most movies.
+### So only use this if the disc isn't copy protected
+#dd if=/dev/diskX of=~/Desktop/myimage.iso
 
-# OR Mac seems to like
+### Mac OS X seems to prefer
+#sudo diskutil unmountDisk /dev/diskX
 
-#sudo diskutil unmountDisk /dev/disk1
+### For copy-protected discs
+### Use dvdbackup to decrypt the contents of the dvd and
+### store the output in a local directory
+#dvdbackup -i /dev/diskX -o ~/Desktop -M -n DISC_NAME
 
-# Alternatively
-# Use dvdbackup to decrypt the contents of the dvd and
-# store the output in a local directory
-#dvdbackup -i /dev/disk1 -o ~/Desktop -M -n DISC_NAME
+### To extract the main feature only, use
+#dvdbackup -i /dev/diskX -o ~/Desktop -F -n DISC_NAME
 
-#########################
-
-# To extract the main feature only, use
-#dvdbackup -i /dev/disk1 -o ~/Desktop -F -n DISC_NAME
-
-# followed by dvdauthor to make DVD files
+### followed by dvdauthor to make DVD files
 #export VIDEO_FORMAT=NTSC
 #dvdauthor -o DISC_NAME -x ~/Desktop/dvd.xml # dvd XML file structure is not trivial
 
-
-#########################
-
-# Makes ISO fs from target directory contents
+### Makes ISO fs from target directory contents
 # mkisofs -V "VOLUME NAME" -dvd-video -udf -o movie.iso DISC_NAME
 
-# To burn DVD, just use
+### To burn DVD, just use
 # cdrecord -v -sao speed=1 movie.iso
-
 
 ### Automated version of script ###
 
 # DvdAuthor 7 and up needs this
 # export VIDEO_FORMAT=NTSC
-# Change to "ntsc" if you'd like to create NTSC disks
+# Change to "ntsc" if you'd like to create NTSC discs
 # format="ntsc"
 
 # Check for dependencies
@@ -57,7 +48,7 @@ done
 if [ $missing = 1 ]
 then
     echo "Please install the missing applications and try again"
-    exit
+    exit 1
 fi
 
 function emphasise() {
@@ -67,36 +58,41 @@ function emphasise() {
 }
 
 # Assign disc volume name to variable
-DISC_NAME=$(df | grep /dev/disk1 | cut -d "/" -f 5)
+DISC_NAME=$(basename `mount | grep udf | cut -d ' ' -f 3`)
+DVD=$(mount | grep udf | cut -d ' ' -f 1)
 
 emphasise "Making a local mirror of disc with dvdbackup"
 
-dvdbackup -i /dev/disk1 -o ~/Desktop/temp -M -n "$DISC_NAME"
+# Make a local decrypted copy of the disc
+dvdbackup -i $DVD -o ~/Desktop/temp -M -n "$DISC_NAME"
+
 if [ $? != 0 ]
 then
 	emphasise "dvdbackup non-zero exit status"
-	exit
+	exit 2
 fi
 
 emphasise "Creating ISO image"
 
 mkisofs -V "$DISC_NAME" -dvd-video -udf -o /Volumes/Multimedia/ISOs/"$DISC_NAME".iso ~/Desktop/temp/"$DISC_NAME"
+
 if [ $? != 0 ]
 then
 	emphasise "mkisofs non-zero exit status"
-	exit
+	exit 3
 fi
 
 emphasise "ISO image created from backup directory"
 
-# Everything passed. Cleanup and burn DVD
-#cdrecord -v -sao speed=1 "$DISC_NAME".iso
-#if [ $? != 0 ]
-#then
-#	emphasise "cdrecord non-zero exit status"
-#	exit
-#fi	
-#rm -rf ~/Desktop/temp/"$DISC_NAME"
-#
-#
-#emphasise "Success! DVD Burned!"
+### Everything passed. Cleanup and burn DVD
+cdrecord -v -sao speed=1 "$DISC_NAME".iso
+if [ $? != 0 ]
+then
+	emphasise "cdrecord non-zero exit status"
+exit 4
+fi
+
+# Cleanup
+rm -rf ~/Desktop/temp/"$DISC_NAME"
+
+emphasise "Success! DVD Burned!"
