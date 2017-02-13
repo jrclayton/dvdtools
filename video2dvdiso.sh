8bc8f0a0e7f33e3c6248221bf474f641c948c8dc
John R. Clayton <jrclayton@yahoo.com>
2017-02-12 21:08:41 -0800

    comment and code cleanup

--- video2dvdiso.sh
+++ video2dvdiso.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
-# AVI or any video 2 DVD iso Script
-# DvdAuthor 7 and up needs this
+#!/usr/bin/env bash
+
+# Script to convert a given video file to the format for dvd then make an iso out of it
+# and optionally burn it to disc for those occasions when you go over to grandmas house
+# or somebody's hooptie home theater only has DVD player, etc. ;-)
+
+# dvdauthor v7+ needs this
 export VIDEO_FORMAT=NTSC
-# Change to "ntsc" if you'd like to create NTSC disks
+
+# Change to "pal" if you'd like to create pal discs
 format="ntsc"
 
 # Check we have enough command line arguments
 if [ $# -lt 1 ]
 then
     echo "Usage: $0 <input file 1 ... input file n>"
-    exit
+    exit 1
 fi
 
 # Check for dependencies
@@ -27,7 +32,7 @@ done
 if [ $missing = 1 ]
 then
     echo "Please install the missing applications and try again"
-    exit
+    exit 2
 fi
 
 function emphasise() {
@@ -42,22 +47,24 @@ do
     if [ ! -e "$var" ]
     then
         echo "File $var not found"
-        exit
+        exit 3
     fi
 done
 
 emphasise "Converting to MPG"
 
+# convert our iput file to the appropriate format for dvd
 for var in "$@"
 do
     ffmpeg -i "$var" -filter:v "scale='if(gt(a,720/480),720,-1)':'if(gt(a,720/480),-1,480)',pad=w=720:h=480:x=(ow-iw)/2:y=(oh-ih)/2" -y -target ${format}-dvd "$var.mpg"
     if [ $? != 0 ]
     then
         emphasise "Conversion failed"
-        exit
+        exit 4
     fi
 done
 
+# makes a 1 titleset dvd xml with our video file
 emphasise "Creating XML file"
 
 echo "<dvdauthor>
@@ -78,22 +85,24 @@ echo "</pgc>
 
 emphasise "Creating DVD contents"
 
+# make the most simple dvd from above folder and xml
 dvdauthor -o dvd -x dvd.xml
 
 if [ $? != 0 ]
 then
     emphasise "DVD Creation failed"
-    exit
+    exit 5
 fi
 
 emphasise "Creating ISO image"
 
+# make the iso from our authored dvd folder
 mkisofs -dvd-video -udf -o movie.iso dvd
 
 if [ $? != 0 ]
 then
     emphasise "ISO Creation failed"
-    exit
+    exit 6
 fi
 
 # Everything passed. Cleanup
@@ -112,7 +121,8 @@ cdrecord -v -sao speed=1 movie.iso
 if [ $? != 0 ]
 then
     emphasise "DVD Burn failed"
-    exit
+    exit 7
 fi
 
 emphasise "Success! DVD Burned!"
+exit 0
