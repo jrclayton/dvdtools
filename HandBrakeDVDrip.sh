df0c8ad164f79346741f66700ee5d01732979d5c
John R. Clayton <jrclayton@yahoo.com>
2017-02-14 18:50:42 -0800

    fixed --iso bug

--- HandBrakeDVDrip.sh
+++ HandBrakeDVDrip.sh
@@ -143,7 +143,7 @@ fi
 # To rip all titles from a VIDEO_TS or .ISO file
 if [ $1 = '--iso' ]; then
     # First get the number of titles on the disk by doing a scan (HB CLI and -t 0) and send STDERR to STDIN so it can be grepped
-    RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i "$1" 2>&1)
+    RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i "$2" 2>&1)
     # Generate numerical list of titles for loops
     TITLES=$(echo $RAWOUT | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3)
 	# return a list of usable titles to plug into a for loop
