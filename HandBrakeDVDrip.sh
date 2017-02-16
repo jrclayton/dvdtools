12dfe611119091ea0207b4804cae15ca0f103705
John R. Clayton <jrclayton@yahoo.com>
2017-02-15 16:34:17 -0800

    Added flexibility to scans -s and -is

--- HandBrakeDVDrip.sh
+++ HandBrakeDVDrip.sh
@@ -6,9 +6,9 @@
 # Check for arguments
 if [[ $# -eq '0' ]] || [[ $# -eq '1' ]] || [[ $# -eq '2' ]] ||
 	[[ $1 = '-a' && $# -ne '3' && $# -ne '4' ]] ||
-	[[ $1 = '-s' && $# -ne '3' && $# -ne '4' ]] ||
+	[[ $1 = '-s' && $# -ne '3' ]] ||
 	[[ $1 = '-i' && $# -ne '4' && $# -ne '5' ]] ||
-	[[ $1 = '--iso' && $# -ne '4' && $# -ne '5' ]] ||
+	[[ $1 = '-is' && $# -ne '4' && $# -ne '5' ]] ||
 	[[ $1 = '-t' && $# -ne '5' && $# -ne '6' ]] ||
 	[[ $1 = '-c' && $# -ne '6' && $# -ne '7' ]] ||
 	[[ $1 = '-cf' && $# -ne '6' && $# -ne '7' ]]
@@ -16,12 +16,12 @@ then
 	echo -e "Usage: dvdrip [switch] MOVIENAME YEAR ... \n \
 	Select mode: either -a for all, -s to scan, -t for titles or -c for chapters or -i to change input source (add 'f' to merge chapters) \n \
 	-a   MOVIENAME YEAR --CLIOPTIONS \n \
-	-s   MOVIENAME YEAR --CLIOPTIONS \n \
+	-s   /path/to/source MOVIENAME \n \
 	-t   MOVIENAME YEAR TITLE_START TITLE_END --CLIOPTIONS \n \
 	-c   MOVIENAME YEAR TITLE START_CHAPTER END_CHAPTER --CLIOPTIONS \n \
 	-cf  MOVIENAME YEAR TITLE START_CHAPTER END_CHAPTER --CLIOPTIONS \n \
 	-i   /path/to/video MOVIENAME YEAR --CLIOPTIONS \n \
-	-iso /path/to/video MOVIENAME YEAR --CLIOPTIONS \n"
+	-is  /path/to/video MODE MOVIENAME YEAR --CLIOPTIONS \n"
 	exit 1
 fi
 
@@ -29,7 +29,7 @@ fi
 DVD=$(mount | grep udf | cut -d ' ' -f 1)
 
 # test for a DVD
-if ! [ $1 = '--iso' ] && ! [ $1 = '-i' ]
+if ! [ $1 = '-is' ] && ! [ $1 = '-i' ] && ! [ $1 = '-s' ]
 then
 	if [ -z "$DVD" ]
 	then
@@ -40,7 +40,7 @@ fi
 
 # make an appropriately named folder on the desktop
 # if --iso; then $3 $4
-if [[ $1 = '--iso' ]]
+if [[ $1 = '-is' ]]
 then
 	mkdir ~/Desktop/"$3"\ \($4\)
 # if other than -i; then $2 $3
@@ -111,18 +111,18 @@ fi
 
 # The following options require a scan before ripping
 
-# output of the scan option
+# To do a simple scan and summary of the DVD or a directory or iso
 if [ $1 = '-s' ]; then
     # First get the number of titles on the disk by doing a scan (HB CLI and -t 0) and send STDERR to STDIN so it can be grepped
-    RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i $DVD 2>&1)
+    RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i $2 2>&1)
     # gives a nice summary of the scan including info about all streams
-	echo "$RAWOUT" | sed -nE '/libhb\: scan thread found [0-9]+ valid title\(s\)/,$p' | tee ~/Desktop/"$2"-\($3\)-scan.txt
+	echo "$RAWOUT" | sed -nE '/libhb\: scan thread found [0-9]+ valid title\(s\)/,$p' | tee ~/Desktop/"$3"-scan.txt
 	exit 0
 fi
 
-# To rip ALL titles
+# To rip ALL titles from a DVD
 if [ $1 = '-a' ]; then
-    # First get the number of titles on the disk by doing a scan (HB CLI and -t 0) and send STDERR to STDIN so it can be grepped
+    # First get the number of titles on the disk by doing a scan
     RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i $DVD 2>&1)
     # Generate numerical list of titles for loops
     TITLES=$(echo "$RAWOUT" | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3)
@@ -140,22 +140,45 @@ if [ $1 = '-a' ]; then
 	exit 0
 fi
 
-# To rip all titles from a VIDEO_TS or .ISO file
-if [ $1 = '--iso' ]; then
-    # First get the number of titles on the disk by doing a scan (HB CLI and -t 0) and send STDERR to STDIN so it can be grepped
-    RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i "$2" 2>&1)
-    # Generate numerical list of titles for loops
-    TITLES=$(echo "$RAWOUT" | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3)
-	# return a list of usable titles to plug into a for loop
-	for i in $TITLES
+# To rip all titles from a structured source (VIDEO_TS or .ISO file) <-- Same as -i but with a scan and loop
+# Also can take DVD/dvd, ISO/iso as FILE/file as $2
+
+if [ $1 = '-is' ]; then
+    if [ $2 = 'dvd' ] || [ $2 = 'DVD' ]; then
+        SOURCE=$DVD
+        # First get the number of titles on the disk by doing a scan
+        RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i $SOURCE 2>&1)
+        # Generate numerical list of titles for loops
+        TITLES=$(echo "$RAWOUT" | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3)
+        # return a list of usable titles to plug into a for loop
+        for t in $TITLES
 	
-	do
-		HandBrakeCLI --verbose=1 --markers --title $i --min-duration 4 --format av_mkv	--encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $5 -i $2 -o ~/Desktop/"$3"\ \($4\)/"$3"\ \($4\)\ -\ $i.mkv
-	done
+        do
+            HandBrakeCLI --verbose=1 --markers --title $t --min-duration 4 --format av_mkv	--encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $5 -i $SOURCE -o ~/Desktop/"$3"\ \($4\)/"$3"\ \($4\)\ -\ $t.mkv
+        done
 	
-	# Notifications
-	echo "Finished ripping all streams from $3"
-	terminal-notifier -message "Finished ripping all streams from $3" -title "Terminal" -subtitle "dvdrip" -sound default -appIcon Documents/Personal_Documents/icons/dvdrip-2.png
-	say -v Samantha "Finished ripping all streams from $3"
-	exit 0
+        # Notifications
+        echo "Finished ripping all streams from $3"
+        terminal-notifier -message "Finished ripping all streams from $3" -title "Terminal" -subtitle "dvdrip" -sound default -appIcon Documents/Personal_Documents/icons/dvdrip-2.png
+        say -v Samantha "Finished ripping all streams from $3"
+        exit 0
+    else
+        SOURCE=$2
+        # First get the number of titles on the disk by doing a scan
+        RAWOUT=$(HandBrakeCLI -t 0 --min-duration 4 -i $SOURCE 2>&1)
+        # Generate numerical list of titles for loops
+        TITLES=$(echo "$RAWOUT" | grep -Eao "\\+ title [0-9]+" | cut -d ' ' -f 3)
+        # return a list of usable titles to plug into a for loop
+        for t in $TITLES
+
+        do
+            HandBrakeCLI --verbose=1 --markers --title $t --min-duration 4 --format av_mkv	--encoder x264 --quality 20.0 --vfr --x264-preset slow --h264-profile high --h264-level 4.0 --audio 1,2,3,4,5,6,7,8,9 --aencoder copy ffaac--audio-copy-mask aac,ac3,dtshd,dts,mp3 --audio-fallback ffac3 --arate Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto,Auto --ab 192,192,192,192,192,192,192,192,192 --decomb --loose-anamorphic --modulus 2 --subtitle scan,1,2,3,4,5,6,7,8,9,10 --native-language eng --subtitle-forced scan $5 -i $SOURCE -o ~/Desktop/"$3"\ \($4\)/"$3"\ \($4\)\ -\ $t.mkv
+        done
+
+        # Notifications
+        echo "Finished ripping all streams from $3"
+        terminal-notifier -message "Finished ripping all streams from $3" -title "Terminal" -subtitle "dvdrip" -sound default -appIcon Documents/Personal_Documents/icons/dvdrip-2.png
+        say -v Samantha "Finished ripping all streams from $3"
+        exit 0
+    fi
 fi
